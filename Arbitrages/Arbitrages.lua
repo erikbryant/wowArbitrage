5de43e806db2c859e3ea9fb3dc662d48ff34c9a2
Erik Bryant <erikbryantology@gmail.com>
2024-07-28 15:23:32 -0400

    Streamline code
    The code is driven by events. These events are blocking. That is, the
    main WoW event loop blocks when any of the registered events are fired.
    If the main event loop is blocked too long the entire app will crash
    (likely, a watchdog timer kills it).
    
    Do what we need to when an event is fired, but do it as quickly as
    possible.

--- Arbitrages/Arbitrages.lua
+++ Arbitrages/Arbitrages.lua
@@ -1,5 +1,8 @@
-local Arbitrages = CreateFrame("Frame")
-local waitingForResponse = true
+-- Enable script error reporting
+C_CVar.SetCVar("scriptErrors", 1)
+
+local Arbitrages = CreateFrame("Frame", "Arbitrages", UIParent)
+Arbitrages:Hide()
 
 -- Print a message with the addon name (in color) as a prefix
 function Arbitrages:Message(...)
@@ -7,33 +10,22 @@ function Arbitrages:Message(...)
     print(prefix, ...)
 end
 
--- Return true if this is an arbitrage opportunity
-function Arbitrages:IsArbitrage(itemID, buyoutPrice)
-    if ItemCache:UnknownID(itemID) or ItemCache:ItemIsEquippable(itemID) then
-        return false
-    end
-    return buyoutPrice < ItemCache:VendorSellPrice(itemID)
-end
-
--- Make an AH favorite of the item
-function Arbitrages:SetAHFavorite(itemID)
-    local itemKey = C_AuctionHouse.MakeItemKey(itemID)
-    C_AuctionHouse.SetFavoriteItem(itemKey, true)
-end
-
--- Scan ReplicateItems to see which auctions qualify as arbitrages
+-- Create an AH favorite for each auction that is an arbitrage
+-- This function is called from OnEvent, so it blocks the main
+-- event loop. Get it done really quickly or the WoW binary will
+-- crash. Inline as much as possible.
 function Arbitrages:FindArbitrages()
-    local foundAny = false
+    local foundArbitrage = false
     for i = 0, C_AuctionHouse.GetNumReplicateItems()-1 do
         local auction = {C_AuctionHouse.GetReplicateItemInfo(i)}
         local buyoutPrice = auction[10]
         local itemID = auction[17]
-        if self:IsArbitrage(itemID, buyoutPrice) then
-            foundAny = true
-            self:SetAHFavorite(itemID)
+        if buyoutPrice < ItemCache:VendorSellPrice(itemID) and not ItemCache:ItemIsEquippable(itemID) and not ItemCache:UnknownID(itemID) then
+            foundArbitrage = true
+            C_AuctionHouse.SetFavoriteItem(C_AuctionHouse.MakeItemKey(itemID), true)
         end
     end
-    if foundAny then
+    if foundArbitrage then
         self:Message("Items loaded into favorites. Enjoy! :)")
     else
         self:Message("No arbitrages found this time. :(")
@@ -43,22 +35,19 @@ end
 -- Dispatch an incoming event
 function Arbitrages:OnEvent(event)
     if event == "AUCTION_HOUSE_SHOW" then
-        self:Message("Sending AH scan request...")
         if C_AuctionHouse.HasFavorites() then
             self:Message("*** Delete your AH favorites! ***")
         end
-        waitingForResponse = true
+        self:Message("Sending AH scan request...")
+        self:RegisterEvent("REPLICATE_ITEM_LIST_UPDATE")
         C_AuctionHouse.ReplicateItems()
     elseif event == "REPLICATE_ITEM_LIST_UPDATE" then
-        if waitingForResponse then
-            waitingForResponse = false
-            self:FindArbitrages()
-        end
+        self:UnregisterEvent("REPLICATE_ITEM_LIST_UPDATE")
+        self:FindArbitrages()
     end
 end
 
 Arbitrages:SetScript("OnEvent", Arbitrages.OnEvent)
 Arbitrages:RegisterEvent("AUCTION_HOUSE_SHOW")
-Arbitrages:RegisterEvent("REPLICATE_ITEM_LIST_UPDATE")
 
 Arbitrages:Message("Loaded and ready to scan!")
