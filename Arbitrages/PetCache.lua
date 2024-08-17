c9e0016454321e98374c5abbc46c5459de667122
Erik Bryant <erikbryantology@gmail.com>
2024-08-16 22:28:59 -0400

    Update cache

--- Arbitrages/PetCache.lua
+++ Arbitrages/PetCache.lua
@@ -1937,6 +1937,7 @@ local OwnedLevelCache = {
   ["Clockwork Rocket Bot"] = 25,
   ["Cloudfeather Fledgling"] = 7,
   ["Cockatiel"] = 4,
+  ["Coilfang Stalker"] = 1,
   ["Comet"] = 7,
   ["Core Hound Pup"] = 9,
   ["Corefire Imp"] = 25,
