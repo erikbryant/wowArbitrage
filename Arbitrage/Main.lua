c2860bc010cbf7e30e4f757c5cea82cd28d3a034
Erik Bryant <erikbryantology@gmail.com>
2024-09-30 16:02:20 -0400

    Factor pet code to separate file

--- Arbitrage/Main.lua
+++ Arbitrage/Main.lua
@@ -2,61 +2,6 @@ local NumAuctionsFoundLastCheck = 0
 local FavoritesCreated = {}
 local Timers = {}
 
--- If this pet auction is a good bargain, add it as an AH favorite
-local function IsCheapPet(auction)
-    local qualityID = auction[4]
-    if qualityID < 3 then
-        -- Not worth buying anything less than Rare(3)
-        return false
-    end
-
-    local name = auction[1]
-    local petLevel = auction[6]
-    local buyoutPrice = auction[10]
-    local ownedLevel = AhaPetCache.OwnedLevel(name)
-
-    -- Is this pet an upgrade?
-    --if petLevel <= ownedLevel then
-    --    -- We already have a better pet
-    --    return
-    --end
-    if ownedLevel > 0 then
-        -- We already have this pet
-        return
-    end
-
-    -- The pet is of interest, but is it cheap?
-    local valueForLevel = 4000000 + 2000000*(petLevel-1)/24
-    if buyoutPrice <= valueForLevel then
-        -- This pet is worth buying!
-        local itemKey = {
-            itemID = auction[17],
-            itemLevel = 0,
-            itemSuffix = 0,
-            battlePetSpeciesID = AhaPetCache.SpeciesId(name),
-        }
-        C_AuctionHouse.SetFavoriteItem(itemKey, true)
-        table.insert(FavoritesCreated, itemKey)
-        AhaUtil.PrettyPrint("Consider buying", name, ownedLevel, "->", petLevel, "@", GetCoinTextureString(buyoutPrice))
-    end
-end
-
--- Create an AH favorite for each pet auction worth buying
-local function FindPetBargains(firstAuction, numAuctions)
-    -- Optimization: Create local function pointers so we only
-    -- search for the function in the global namespace once,
-    -- instead of on every call.
-    local getReplicateItemInfo = C_AuctionHouse.GetReplicateItemInfo
-
-    for i = firstAuction, numAuctions-1 do
-        local auction = {getReplicateItemInfo(i)}
-        -- auction[17] is the itemID
-        if auction[17] == 82800 then
-            IsCheapPet(auction)
-        end
-    end
-end
-
 -- Create an AH favorite for each auction that is an arbitrage
 local function FindArbitrages(firstAuction, numAuctions)
     -- How loaded is the AH? At its lightest load it can do almost 200,000 auctions
@@ -85,11 +30,11 @@ local function FindArbitrages(firstAuction, numAuctions)
             }
             C_AuctionHouse.SetFavoriteItem(itemKey, true)
             table.insert(FavoritesCreated, itemKey)
-            PlaySound(SOUNDKIT.AUCTION_WINDOW_CLOSE);
         end
     end
 
     if foundArbitrage then
+        PlaySound(SOUNDKIT.AUCTION_WINDOW_CLOSE)
         AhaUtil.PrettyPrint("Arbitrage auctions found and added to favorites!")
     end
 end
@@ -108,8 +53,8 @@ local function CheckForAuctionResults()
             NumAuctionsFoundLastCheck = 0
         end
         FindArbitrages(NumAuctionsFoundLastCheck, numAuctions)
-        -- Disabled because I am not searching for pets right now
-        --FindPetBargains(NumAuctionsFoundLastCheck, numAuctions)
+        -- << Disabled because I am not searching for pets right now >>
+        --AhaPets.FindBargains(NumAuctionsFoundLastCheck, numAuctions)
     end
 
     NumAuctionsFoundLastCheck = numAuctions
---
+++ Arbitrage/Pets.lua
@@ -0,0 +1,58 @@
+-- If this pet auction is a good bargain, add it as an AH favorite
+local function IsCheapPet(auction)
+    local qualityID = auction[4]
+    if qualityID < 3 then
+        -- Not worth buying anything less than Rare(3)
+        return false
+    end
+
+    local name = auction[1]
+    local petLevel = auction[6]
+    local buyoutPrice = auction[10]
+    local ownedLevel = AhaPetCache.OwnedLevel(name)
+
+    -- Is this pet an upgrade?
+    --if petLevel <= ownedLevel then
+    --    -- We already have a better pet
+    --    return
+    --end
+    if ownedLevel > 0 then
+        -- We already have this pet
+        return
+    end
+
+    -- The pet is of interest, but is it cheap?
+    local valueForLevel = 4000000 + 2000000*(petLevel-1)/24
+    if buyoutPrice <= valueForLevel then
+        -- This pet is worth buying!
+        local itemKey = {
+            itemID = auction[17],
+            itemLevel = 0,
+            itemSuffix = 0,
+            battlePetSpeciesID = AhaPetCache.SpeciesId(name),
+        }
+        C_AuctionHouse.SetFavoriteItem(itemKey, true)
+        table.insert(FavoritesCreated, itemKey)
+        AhaUtil.PrettyPrint("Consider buying", name, ownedLevel, "->", petLevel, "@", GetCoinTextureString(buyoutPrice))
+    end
+end
+
+-- Create an AH favorite for each pet auction worth buying
+local function FindBargains(firstAuction, numAuctions)
+    -- Optimization: Create local function pointers so we only
+    -- search for the function in the global namespace once,
+    -- instead of on every call.
+    local getReplicateItemInfo = C_AuctionHouse.GetReplicateItemInfo
+
+    for i = firstAuction, numAuctions-1 do
+        local auction = {getReplicateItemInfo(i)}
+        -- auction[17] is the itemID
+        if auction[17] == 82800 then
+            IsCheapPet(auction)
+        end
+    end
+end
+
+AhaPets = {
+    FindBargains = FindBargains,
+}
